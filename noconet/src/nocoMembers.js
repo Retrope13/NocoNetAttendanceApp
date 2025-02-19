ea264fc27e8712650b4944ab9aad286196510ba9
Sam McKay <sammckay31@gmail.com>
2025-02-19 16:53:01 -0700

    The current list of noconet members. Will be appending to this or putting it in a db idk

---
+++ noconet/src/nocoMembers.js
@@ -0,0 +1,7 @@
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const nocoMembers = [
+    { label: 'Sam McKay', email: "mckaypable@gmail.com", phoneNumber: 4095048250 },
+  ];
+  
+  export default nocoMembers;
+  
